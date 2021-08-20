7a2f72754eff208e415977ef50684dc06a94d693
Seb <88947631+sebeeeeeee@users.noreply.github.com>
2021-08-20 23:45:09 +0200

    Node.js is pain

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 import shutil
 
-
+# You will have to change the node_modules path. Found a better solution? Make a PR on the repository!
 try:
     with shutil.rmtree("/Users/username/folder/project/node_modules", os.remove("package-lock.json")) as f:
         print("Hippity hoppity, hippty hee, node.js has commited alt+f4")
@@ -12,5 +12,6 @@ except FileNotFoundError:
 
 
 print()
+print('- https://github.com/sebeeeeeee/painful-nodejs')
 print("- You should check out my Github! https://github.com/sebeeeeeee")
 
