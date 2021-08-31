48a5defe1ed361f0dbcbdf0afb0ac984fbfb09e7
Seb <88947631+sebeeeeeee@users.noreply.github.com>
2021-08-31 19:03:15 +0200

    eeeeeeee

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import shutil
 # You will have to change the node_modules path. Found a better solution? Make a PR on the repository!
 try:
     with shutil.rmtree("/Users/username/folder/project/node_modules", os.remove("package-lock.json")) as f:
-        print("Hippity hoppity, hippty hee, node.js has commited alt+f4")
+        print("Hippity hoppity, hippty hee, ur code has commited eeeeeeee")
 except FileNotFoundError:
     print('- "No files found here" said the code')
 
