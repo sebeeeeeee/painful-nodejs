501f0bd438ffc31513732e8c20e12c209a2de990
Seb <88947631+sebeeeeeee@users.noreply.github.com>
2021-09-05 12:45:48 +0200

    yarn exists too for some reason

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import shutil
 
 # You will have to change the node_modules path. Found a better solution? Make a PR on the repository!
 try:
-    with shutil.rmtree("/Users/username/folder/project/node_modules", os.remove("package-lock.json")) as f:
+    with shutil.rmtree("/Users/username/folder/project/node_modules", os.remove("package-lock.json")), os.remove("yarn.lock")) as f:
         print("Hippity hoppity, hippty hee, ur code has commited eeeeeeee")
 except FileNotFoundError:
     print('- "No files found here" said the code')
